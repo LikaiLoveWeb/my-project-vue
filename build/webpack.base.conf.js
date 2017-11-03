1389bb992dbf90e70733eec0da076f66d5fccc6a
likai <likai_loveweb@163.com>
2017-11-03 17:33:29 +0800

    完善部分代码~

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -4,12 +4,34 @@ const utils = require('./utils')
 const config = require('../config')
 const vueLoaderConfig = require('./vue-loader.conf');
 const glob = require('glob');
+const pkg=require("../package.json");
 const webpack = require('webpack');
 const _ = require(resolve('libs')+'/js/lodash.min');
 
 function resolve (dir) {
   return path.join(__dirname, '..', dir)
 }
+
+/*
+ * create html
+ * （创建html文件）
+ * */
+let HtmlWebpackPlugin = require('html-webpack-plugin');
+// var HtmlWebpackAvalonPlugin = require('./webpack-html-webpack-plugin-avalon.config.js');
+
+
+/*
+ * extract css
+ * （提取css文件）
+ * */
+let ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+/*
+ * 压缩css文件
+ */
+let OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+
+
 
 /**
  * 获得路径
@@ -37,13 +59,23 @@ function getEntry(globPath, pathDir) {
   return entries;
 }
 
+//获取多模块入口css文件
+let file_css = getEntry(resolve('src')+'/apps/css/**/*.?(css|scss|less)', resolve('src')+'/apps/css/');
+
+//获取图片文件
+let file_pic = getEntry(resolve('src')+'/apps/img/**/*.?(png|jpg|jpeg|gif)', resolve('src')+'/apps/img/');
+
+//获取vue组件
+let file_components = getEntry(resolve('src')+'/components/**/*.?(js|vue|html)', resolve('src')+'/components/');
+
+//获取多模块入口js文件
+let file_js = getEntry(resolve('src')+'/apps/js/**/*.js', resolve('src')+'/apps/js/');
+
+
 //获取多模块入口html文件
-let file_html = getEntry('./src/apps/html/**/*.html', './src/html/');
+let file_html = getEntry('./src/apps/html/**/*.html', './src/apps/html/');
 //获取路由路径
 let pages = Object.keys(file_html);
-
-console.log(pages);
-
 
 
 let output = {
@@ -72,6 +104,13 @@ let output = {
   // chunkFilename: 'js/[name].js'
 
 };
+
+
+let tempKeys = _.union(pages,Object.keys(file_js),Object.keys(file_pic),Object.keys(file_components));
+let tempCollection = [];
+tempCollection.push(file_js);
+tempCollection.push(file_pic);
+tempCollection.push(file_components);
 let tempArray = {};
 function join(array, separator) {
   let nativeJoin = Array.prototype.join;
@@ -90,6 +129,107 @@ let entry = Object.assign(tempArray, {
   // 用到什么公共lib（例如jquery.js），就把它加进common去，目的是将公用库单独提取打包
   common: ['vue','vue-router','jquery','lodash']
 });
+
+let HtmlWebpackPluginArray = [
+  //第三方插件暴漏出来
+  new webpack.ProvidePlugin({
+    $: "jquery",
+    jQuery: "jquery",
+    "window.jQuery": "jquery",
+    "_": "lodash"
+  }),
+  /*
+   * gloabal flag
+   * （全局标识）
+   * */
+  new webpack.DefinePlugin({
+    /*
+     * 路由路径
+     */
+    __PAGES__: JSON.stringify({
+      'v':join(pages,',')
+    }),
+
+    __APIURL__: JSON.stringify({
+      'v':pkg.config.APIUrl
+    })
+  }),
+
+
+  /*
+   * common js
+   * （公共js）
+   * */
+  new webpack.optimize.CommonsChunkPlugin(
+    {names: ["common", "webpackAssets"]}
+  ),
+
+
+  /*
+   * extract css
+   * （提取css文件到单独的文件中）
+   */
+  new ExtractTextPlugin("css/[name].css", {allChunks: true}),
+
+  /*
+   *create html file
+   * （创建html文件）
+   * */
+  new HtmlWebpackPlugin({
+    filename: 'index.html',
+    template: resolve('src') + '/index.html',
+    /*
+     * inject: true | 'head' | 'body' | false Inject all assets into the given template or templateContent -
+     * When passing true or 'body' all javascript resources will be placed at the bottom of the body element.
+     * 'head' will place the scripts in the head element.
+     * */
+    inject: 'body',
+
+    // 需要依赖的模块
+    chunks: ['common', 'index', 'webpackAssets'],
+
+    // 根据依赖自动排序
+    chunksSortMode: 'dependency',
+    minify: {
+      removeComments: true,//移除HTML中的注释
+      collapseWhitespace: true,//删除空白符与换行符
+      collapseInlineTagWhitespace: true,//display:inline;折叠时不要在元素之间留下任何空格。
+      removeRedundantAttributes: true,//当值与默认值匹配时，删除属性。
+      useShortDoctype: true,//替换为doctype（HTML5）短文型
+      removeEmptyAttributes: true,//使用仅含空格的值删除所有属性
+      removeStyleLinkTypeAttributes: true,//type="text/css"从中删除style和link标记。其他type属性值保持不变
+      keepClosingSlash: true//在单体元素上保持斜线
+    }
+  })
+];
+//html 模板插件
+pages.forEach(function(pathname) {
+  let conf = {
+    filename: 'html/' + pathname + '.html', //生成的html存放路径，相对于path
+    template: resolve('src')+'/apps/html/' + pathname + '.html', //html模板路径
+    inject: false,
+    // template: path.resolve(__dirname, './src/index.html'), //html模板路径
+    // favicon:'./src/img/imglogo.ico', //favicon路径
+    // inject: 'body',
+    // 需要依赖的模块
+    // chunks: ['common', 'index',pathname+'Start', 'webpackAssets'],
+    // 根据依赖自动排序
+    // chunksSortMode: 'dependency'
+    /*
+     * 压缩这块，调用了html-minify，会导致压缩时候的很多html语法检查问题，
+     * 如在html标签属性上使用{{...}}表达式，很多情况下并不需要在此配置压缩项，
+     * 另外，UglifyJsPlugin会在压缩代码的时候连同html一起压缩。
+     * 为避免压缩html，需要在html-loader上配置'html?-minimize'，见loaders中html-loader的配置。
+     */
+    minify: { //压缩HTML文件
+      removeComments: true, //移除HTML中的注释
+      collapseWhitespace: true,//删除空白符与换行符
+      collapseInlineTagWhitespace: true//display:inline;折叠时不要在元素之间留下任何空格。
+    }
+  };
+  HtmlWebpackPluginArray.push(new HtmlWebpackPlugin(conf));
+});
+
 
 module.exports = {
   entry: entry,
@@ -146,12 +286,5 @@ module.exports = {
     ]
   },
   // 配置插件项
-  plugins: [
-    new webpack.ProvidePlugin({
-      $: "jquery",
-      jQuery: "jquery",
-      "window.jQuery": "jquery",
-      "_": "lodash"
-    }),
-  ]
+  plugins: HtmlWebpackPluginArray
 };
--- src/MainRoad/index/index.vue
+++ src/MainRoad/index/index.vue
@@ -31,11 +31,17 @@
     components:{HeaderName,FooterName},
     mounted:function () {
       //el 被新创建的 vm.$el 替换，并挂载到实例上去之后调用该钩子
-      console.log($('div'));
+      console.log($('div'),'jquery 已经成功引入');
+      let code = true;
+      if(_.isEmpty(code)){
+        console.log('验证第三方插件lodash是否生效')
+      }else {
+        console.log('验证第三方插件lodash是否生效')
+      }
     },
     methods:{
       goPage:function () {
-        window.location.hash = '/page'
+       window.location.hash = '/page'
       },
       myEventClick:function (msg) {
        this.msg = msg;
